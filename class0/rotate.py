408cbcd96715db17461ef44091aa5624f155e122
CharlotteHonG <hunandy14@gmail.com>
2017-01-16 15:33:32 +0800

    效能優化

--- class0/rotate.py
+++ class0/rotate.py
@@ -4,10 +4,10 @@
 from __future__ import print_function
 # ===============================================================
 # 筆記：
-# 1. 跑幾次的偵測
-# 2. 找最長邊
-# 3. 改良任意點開始繞
-# 4. 反繞
+# 1. [v]跑幾次的偵測
+# 2. [v]找最長邊
+# 3. []改良任意點開始繞
+# 4. []反繞
 class Rotate:
     # 初始化
     def __init__(self, height, width):
@@ -18,8 +18,10 @@ class Rotate:
         self.arr = [[-1]*width for x in xrange(height)]
         self.idx_y, self.idx_x = -1, -1
         self.star = 0
+        self.num = self.star
         # 最長邊
-
+        # self.len
+        return
     # 印出
     def pri(self):
         for y in xrange(self.height):
@@ -29,6 +31,7 @@ class Rotate:
                     self.arr[y][x]), end=('')
                 );
             print()
+        return
     # 核心程式
     def run_core(self, director):
         # print("Run")
@@ -38,11 +41,16 @@ class Rotate:
         if self.idx_x==-1:
             self.idx_x=0
         # 依照最長邊跑
-        for x in xrange(self.width):
+        for x in xrange(max(self.height, self.width)):
+            # 已完成則跳出
+            if (self.height*self.width-1+self.star==self.num-1):
+                # print("Finish")
+                return
+            # print("Go")
             # 有效空值才填入
             if self.arr[self.idx_y][self.idx_x]==-1:
-                self.arr[self.idx_y][self.idx_x]=self.star
-                self.star += 1
+                self.arr[self.idx_y][self.idx_x]=self.num
+                self.num += 1
             # 一方向走
             self.idx_y = self.idx_y + director[0]
             self.idx_x = self.idx_x + director[1]
@@ -50,25 +58,30 @@ class Rotate:
             try:
                 # 0越界
                 if self.arr[self.idx_y][self.idx_x] != -1:
+                    # print ("0越界")
                     # 0越界- 補償
-                    self.idx_y, self.idx_x = \
-                        self.idx_y-diry, self.idx_x-dirx
+                    self.idx_y, self.idx_x = self.idx_y-diry, self.idx_x-dirx
             # 長度越界
             except:
+                # print("長度越界")
                 # 長度越界 - 補償
                 self.idx_y, self.idx_x = \
                     self.idx_y-director[0], \
                     self.idx_x-director[1]
+                return
+        return
     # 跑一圈
     def run_once(self):
         self.run_core([0,1])
         self.run_core([1,0])
         self.run_core([0,-1])
         self.run_core([-1,0])
+        return
     # 跑全部
     def run(self):
-        for x in xrange( int(round(self.width/2))+1 ):
+        for x in xrange(min(self.height, self.width)/2+1):
             self.run_once()
+        return
 # ===============================================================
 # 主程式
 def main():
