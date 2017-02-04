b30f401abccb52d372c301c65a4ad2f3b4b734ea
Charlotte.HonG <hunandy14@gmail.com>
2017-02-04 22:17:23 +0800

    Add MultiFiles

---
+++ class4/MultiFile/Suorce.hpp
@@ -0,0 +1,16 @@
+/*****************************************************************
+Name : 
+Date : 2017/02/04
+By   : CharlotteHonG
+Final: 2017/02/04
+*****************************************************************/
+#ifndef SOURCE_HPP
+#define SOURCE_HPP
+
+
+#include <iostream>
+using namespace std;
+void pri(int &);
+
+
+#endif
