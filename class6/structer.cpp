dafa37de9c1f1212eb38ed0b7d2b1f57a0393aaf
Charlotte.HonG <hunandy14@gmail.com>
2017-02-14 16:44:50 +0800

    拆檔寫法

--- class6/structer.cpp
+++ class6/structer.cpp
@@ -7,6 +7,7 @@ Final: 2017/02/14
 #include <iostream>
 using namespace std;
 
+// hpp
 class SingleLink{
 public:
     // 建構子
@@ -14,12 +15,14 @@ public:
     : num(n){}
 public:
     // 方法
-    void pri(){
-        cout << "num=" << this->num << endl;
-    }
+    void pri();
 private:
     int num;
 };
+// cpp
+void SingleLink::pri(){
+    cout << "num=" << this->num << endl;
+}
 /*==============================================================*/
 int main(int argc, char const *argv[]){
     SingleLink a(7);
