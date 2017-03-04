6ea36da56b8b11831af478c4610a3548830764a4
CharlotteHonG <hunandy14@gmail.com>
2017-03-04 13:47:42 +0800

    uypdate

--- class7/operator[].cpp
+++ class7/operator[].cpp
@@ -22,20 +22,18 @@ public:
             cout << i << ", ";
         }cout << endl;
     }
+    // 一開始的存取方式
     void set(int idx,int num){
         arr[idx]=num;
     }
     int & at(int idx){
         return arr[idx];
     }
+    // 重載下標
     int & operator[](size_t idx){
         return arr[idx];
     }
-    void operator=(Arr rhs){
-        for(unsigned i = 0; i < arr.size(); ++i) {
-            arr[i]=rhs[i];
-        }
-    }
+    // 重載 = 
     void operator=(int rhs){
         for(unsigned i = 0; i < arr.size(); ++i) {
             arr[i]=rhs;
@@ -51,20 +49,16 @@ int main(int argc, char const *argv[]){
     // 三個等價
     a.set(0, 7);
     a.at(0)=7;
-    a[0]=7;
+    a[0]=7; //a.operator[](7);
+
     // 印出
     for(unsigned i = 0; i < 5; ++i) {
         cout << a[i]<< ", ";
         // cout << a.operator[](i)<< endl;
     } cout << endl;
-
-
-    b=a;
-    // b.operator=(a);
-    b.pri("b=a");
     
-    b=3;
-    // b.operator=(3);
+    // 重載等於
+    b=3; // b.operator=(3);
     b.pri("b=1");
 
     return 0;
